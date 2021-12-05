adbf23b0e88b0204967d41ef0fca2df96c47e392
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2021-12-05 22:27:09 +0200

    Add files via upload

---
+++ Tank Robot V2/UltraSonic.ino
@@ -0,0 +1,37 @@
+
+int trigPin = 5;            // Trigger
+int echoPin = 4;            // Echo
+long duration, cm;
+
+
+void setup() {
+  
+  Serial.begin (9600);      //Serial Port begin
+  
+  pinMode(trigPin, OUTPUT); // Посылает ультразвуковой сигнал
+  pinMode(echoPin, INPUT);  // Принимает сигнал
+}
+
+
+void loop() {
+  
+  digitalWrite(trigPin, LOW);  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH); // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+   
+  // duration is the time (in microseconds) from the sending  of the ping to the reception of its echo off of an object.
+  // продолжительность - сколько времени прошло с того момента как отослали сигнал(Trig) и когда его получили(echo) в микросекундах 
+  
+  duration = pulseIn(echoPin, HIGH);    // Read the signal from the sensor: a HIGH pulse whose
+  
+  // Convert the time into a distance
+  cm = (duration/2) * 0.0343;     // Divide by 29.1 or multiply by 0.0343(расстояние = время * скорость) скорость звука 343 м/с
+  
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(250);
+}
