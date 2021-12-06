c3c0d8d34915e26c5fbf4fae0e80a7bf53d880f7
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2021-12-06 20:30:41 +0200

    Bluetooth signal affect  led

---
+++ Tank Robot V2/Bluetooth_Led.ino
@@ -0,0 +1,27 @@
+int ledpin = 10;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledpin, OUTPUT);
+}
+
+void loop()
+{
+  int i;
+  if (Serial.available())             // если поступил сигнал, то активизируется цикл
+  {
+    i = Serial.read();                // переменная i получает значение сигнала
+    Serial.println("DATA RECEIVED:"); 
+    if (i == '1')                     // если полученное значение равно "1" зажигаем led
+    {
+      digitalWrite(ledpin, 1);
+      Serial.println("led on");
+    }
+    if (i == '0')                     // если полученное значение равно "0" гасим led
+    {
+      digitalWrite(ledpin, 0);
+      Serial.println("led off");
+    }
+  }
+}
