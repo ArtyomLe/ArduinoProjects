74345deedbb63f663fcb2c66ed8ad1d99fa2323d
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2021-12-06 14:35:14 +0200

    Led control via ir remote

---
+++ Tank Robot V2/IR_Led.ino
@@ -0,0 +1,33 @@
+#include <IRremoteTank.h>
+int RECV_PIN = A0;                                // define the pin of IR receiver as A0
+int LED_PIN = 10;                                 // define the pin of LED
+int a = 0;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup()
+{
+    Serial.begin(9600);
+    irrecv.enableIRIn();                          // initialize the IR receiver
+    pinMode(LED_PIN, OUTPUT);                     // set LED_PIN to OUTPUT
+}
+
+void loop()
+{
+    if (irrecv.decode(&results))
+    {
+        if (results.value == 0xFF02FD & a == 0)   // according to chart, press "OK" button on remote control, LED will be controled   
+        {
+            digitalWrite(LED_PIN, HIGH);          // LED will be on
+            a = 1;
+        }
+        elif (results.value == 0xFF02FD & a == 1) // press again
+        {
+            digitalWrite(LED_PIN, LOW);           // LED will go off
+	    a = 0;   
+        }
+         
+        irrecv.resume();                          // recieve the next value
+    }   
+}
