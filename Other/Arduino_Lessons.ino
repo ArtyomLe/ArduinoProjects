4231bdae224e219ff4766604c44114554989019d
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2021-12-16 21:45:32 +0200

    Update Arduino_Lessons.ino

--- Other/Arduino_Lessons.ino
+++ Other/Arduino_Lessons.ino
@@ -116,3 +116,20 @@ void loop() {
 /********************************************************/
 
 
+void setup() {
+  pinMode(A5, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  digitalWrite(13, HIGH);
+  digitalWrite(12, 1);
+  digitalWrite(A5, 0);
+}
+
+void loop() {
+
+  
+}
+/******************************************************/
+
+
