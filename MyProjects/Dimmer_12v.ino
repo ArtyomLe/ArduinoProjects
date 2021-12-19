ffbc082166e60be17d83aec7f1aefb5ea80a019a
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2021-12-19 23:13:38 +0200

    Мерцание

--- MyProjects/Dimmer_12v.ino
+++ MyProjects/Dimmer_12v.ino
@@ -12,3 +12,23 @@ void loop() {
   analogWrite(5, value);               // Выход ШИМ сигнала на транзистор 
   Serial.println(value);
 }
+/*
+ * #define pot 0                           // Вход потинциометра
+int value; 
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (value = 0; value < 255; value++) {
+    analogWrite(5, value);
+    delay(10);
+  }
+  for (value = 255; value > 0; value--) {
+    analogWrite(5, value);
+    delay(10);
+  }
+}
+ */
