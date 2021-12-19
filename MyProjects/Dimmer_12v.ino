1d75625bfd1096aa17a1c9b883451557478439dc
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2021-12-19 23:32:36 +0200

    Мерцание в зависимости от показания потенциометра

--- MyProjects/Dimmer_12v.ino
+++ MyProjects/Dimmer_12v.ino
@@ -12,6 +12,7 @@ void loop() {
   analogWrite(5, value);               // Выход ШИМ сигнала на транзистор 
   Serial.println(value);
 }
+/*******Мерцание*************************************************************************/
 /*
  * #define pot 0                           // Вход потинциометра
 int value; 
@@ -32,3 +33,26 @@ void loop() {
   }
 }
  */
+/*****Мерцание в зависимости от показания потенциометра*********************************/
+/*
+ * #define pot 0                           // Вход потинциометра
+int value; 
+int i;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  value = analogRead(pot);
+  value = map(value, 0, 1023, 0, 255);
+  value = constrain(value, 0, 255);
+  for(i = 0; i < value; i++) {
+    analogWrite(5, i);
+    delay(10);
+  }
+  for(i = value; i > 0; i--) {
+    analogWrite(5, i);
+    delay(10);
+  }
+}
