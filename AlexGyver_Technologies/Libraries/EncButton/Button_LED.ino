6914038624da0aa36c39cfe0dfa2a6262d3b27e3
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2021-12-25 18:18:06 +0200

    Two buttons

--- AlexGyver_Technologies/Libraries/EncButton/Button_LED.ino
+++ AlexGyver_Technologies/Libraries/EncButton/Button_LED.ino
@@ -9,3 +9,25 @@ void loop() {
   btn1.tick();
   if(btn1.isClick()) digitalWrite(13, !digitalRead(13));
 }
+
+/*******************TWO BUTTONS**************************/
+#include <EncButton.h>
+EncButton<EB_TICK, 2> btn1(INPUT_PULLUP);
+EncButton<EB_TICK, 3> btn2(INPUT_PULLUP);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  btn1.tick();
+  btn2.tick();
+
+  if(btn1.isClick()) Serial.println("btn 1 is clicked!");
+  if(btn1.isHolded()) Serial.println("btn 1 is holded!");
+
+  if(btn2.isClick()) digitalWrite(13, !digitalRead(13)),
+  Serial.println("btn 2 switch LED");
+}
+/**************************************************/
