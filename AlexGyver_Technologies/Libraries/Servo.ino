bdb2230911d5cd3b6550d32cab7c0e5f16b6e48f
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2021-12-29 12:50:52 +0200

    Раз в секунду задаем угол положения серво

--- AlexGyver_Technologies/Libraries/Servo.ino
+++ AlexGyver_Technologies/Libraries/Servo.ino
@@ -1,1 +1,23 @@
-/
+/***************Раз в секунду задаем угол положения серво**********************/
+
+#include <Servo.h>
+Servo servo;
+
+void setup() {
+  servo.attach(12);
+}
+
+void loop() {
+  static uint32_t tmr;
+  static byte mode = 0;
+  if(millis() - tmr >= 1000) {
+    tmr = millis();
+    if(++mode >= 4) mode = 0;
+    switch (mode) {
+      case 0: servo.write(10); break;
+      case 1: servo.write(50); break;
+      case 2: servo.write(120); break;
+      case 3: servo.write(170); break;
+ } 
+}
+/***************Раз в секунду задаем угол положения серво**********************/
