b4c8fdca55851ea9388b335df01840d354423992
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2021-12-29 12:57:19 +0200

    Каждому углу соответствует своё время таймера

--- AlexGyver_Technologies/Libraries/Servo.ino
+++ AlexGyver_Technologies/Libraries/Servo.ino
@@ -18,6 +18,32 @@ void loop() {
       case 1: servo.write(50); break;
       case 2: servo.write(120); break;
       case 3: servo.write(170); break;
+  } 
+ }
+}
+
+/*********Каждому углу соответствует своё время таймера*************************/
+
+  #include <Servo.h>
+Servo servo;
+
+void setup() {
+  servo.attach(12);
+}
+
+void loop() {
+  static uint32_t tmr, period;
+  static byte mode = 0;
+  if(millis() - tmr >= period) {
+    tmr = millis();
+    if(++mode >= 4) mode = 0;
+    switch (mode) {
+      case 0: servo.write(10); period = 200; break;
+      case 1: servo.write(50); period = 400; break;
+      case 2: servo.write(120); period = 800; break;
+      case 3: servo.write(170); period = 1000; break;
+  } 
  } 
 }
-/*********Каждому углу соответствует своё время таймера*************************/
+
+/**************************************************************************/
