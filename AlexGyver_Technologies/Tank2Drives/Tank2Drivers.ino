bbf66ea3faaa375fb4ead8263ab1d42466dca070
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2022-01-06 13:12:00 +0200

    Servo attach

--- AlexGyver_Technologies/Tank2Drives/Tank2Drivers.ino
+++ AlexGyver_Technologies/Tank2Drives/Tank2Drivers.ino
@@ -1,12 +1,17 @@
 // мин. сигнал, при котором мотор начинает вращение
 #define MIN_DUTY 120
+#define MAX_CAM_SPEED 40                                                           // ===============
+
+// пределы поворота
+#define CAM_MIN 20                                                                 // ===============
+#define CAM_MAX 160                                                                // ===============
 
 // пины драйвера
 #define MOT_RA 2
 #define MOT_RB 3
 #define MOT_LA 4
 #define MOT_LB 5
-//#define SERVO_CAM 8
+#define SERVO_CAM 8                                                                // ===============
 
 // пины ресивера ps2
 #define PS2_DAT A0
@@ -15,8 +20,8 @@
 #define PS2_CLK A3
 
 // ===========================
-//#include <Servo.h>
-//Servo cam;
+#include <Servo.h>                                                                // ===============
+Servo cam;                                                                        // ===============
 #include <GyverMotor.h>
 // (тип, пин, ШИМ пин, уровень)
 GMotor motorR(DRIVER2WIRE, MOT_RA, MOT_RB, HIGH);
@@ -24,14 +29,14 @@ GMotor motorL(DRIVER2WIRE, MOT_LA, MOT_LB, HIGH);
 
 #include <PS2X_lib.h>
 PS2X ps2x;
-  // позиция привода (градусы * 10)
-  //int camP = 0;
+позиция привода (градусы * 10)                                                    // ===============
+int camP = 0;                                                                     // ===============
 
 void setup() {
   Serial.begin(9600);
   
-  //cam.attach(SERVO_CAM);      // подключаем
-  //cam.write(camP);            // ставим на начальные позиции
+  cam.attach(SERVO_CAM);      // подключаем серво                                 // ================
+  cam.write(camP);            // ставим на начальные позиции                      // ================
 
   motorR.setMode(AUTO);
   motorL.setMode(AUTO);
@@ -60,14 +65,18 @@ void loop() {
   ps2x.reconfig_gamepad();                                  // костыль https://stackoverflow.com/questions/46493222/why-arduino-needs-to-be-restarted-after-ps2-controller-communication-in-arduino
   
   if (success) {
-   /* 
+  //===================================================== 
     // таймер на 30 мс (30 раз в секунду)
     static uint32_t tmr;
     if (success && millis() - tmr >= 30) {
       tmr = millis();
       
-    camP += map(ps2x.Analog(PSS_RX), 0, 255, MAX_ARM_SPEED, -MAX_ARM_SPEED);
-    */
+    camP += map(ps2x.Analog(PSS_LX), 0, 255, MAX_CAM_SPEED, -MAX_CAM_SPEED);
+    camP = constrain(camP, CAM_MIN, CAM_MAX);                // ограничиваем скорость
+    Serial.print(camP);
+    cam.write(camP / 10);                                    // переводим в градусы и применяем
+    }
+  //=====================================================
     // преобразуем стики от 0..255 к -255, 255
     int LX = map(ps2x.Analog(PSS_RX), 255, 0, -255, 255);   // Если едет правый борт ver1.  map(ps2x.Analog(PSS_LX), 256, 0, -255, 255);
     int LY = map(ps2x.Analog(PSS_RY), 255, 0, -255, 255);   // Если едет правый борт ver1.  map(ps2x.Analog(PSS_LY), 256, 0, -255, 255);
@@ -76,7 +85,7 @@ void loop() {
      RX = 0;
      RY = 0;
     }
-      
+  //======================================================    
   // Show up stick values via Serial_print (при нажатии L1 или R1 выводятся состояния обоих аналоговых стиков.)
   if(ps2x.Button(PSB_L1) || ps2x.Button(PSB_R1))            // print stick values if either is TRUE
   {
@@ -89,10 +98,10 @@ void loop() {
     Serial.print(“,”);
     Serial.println(ps2x.Analog(PSS_RX), DEC); 
   } 
-
+ //=======================================================
     // танковая схема
-    int dutyR = LY + LX;
-    int dutyL = LY - LX;
+    int dutyR = RY + RX;
+    int dutyL = RY - RX;
     dutyR = constrain(dutyR, -255, 255);
     dutyL = constrain(dutyL, -255, 255);
 
