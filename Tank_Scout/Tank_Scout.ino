1aded5259ab00f0827bd826669b4ecf4d29f00ca
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2022-01-09 09:51:32 +0200

    Update Tank_Scout.ino

--- Tank_Scout/Tank_Scout.ino
+++ Tank_Scout/Tank_Scout.ino
@@ -146,3 +146,37 @@ float getDist() {
   return (us / 58.2);
   
 }
+
+
+/*
+//==============OLED addition=====================
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup() {
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+}
+
+void loop() {
+
+  Distance();
+  display.display();
+}
+
+void Distance() {
+
+  delay(2000);
+  
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,4);
+  display.print("Distance:    ");
+  display.print(dist);
+  display.print(" cm");
+}
+*/
