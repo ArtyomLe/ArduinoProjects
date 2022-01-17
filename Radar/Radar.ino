48a6d7ba19550f2a06fb4d2dba26f640a74b1c08
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2022-01-17 22:48:14 +0200

    Update Radar.ino

--- Radar/Radar.ino
+++ Radar/Radar.ino
@@ -14,31 +14,31 @@ int distance;
 Servo myServo;                     // Creates a servo object for controlling the servo motor
 
   uint32_t tmr;
-  int val = 0;
-  int dir = 1;
+  int i = 10;                      // Set angle to start from 
+  int dir = 1;                     // Set step (increase number = high rotation)
 
 void setup() {
   pinMode(trigPin, OUTPUT);        // Sets the trigPin as an Output
   pinMode(echoPin, INPUT);         // Sets the echoPin as an Input
   Serial.begin(9600);
-  myServo.attach(3);              // Defines on which pin is the servo motor attached
+  myServo.attach(3);               // Defines on which pin is the servo motor attached
 }
                             
 
 void loop() {
 
-  if (millis() - tmr >= 15) {                 // Instead of delay function
+  if (millis() - tmr >= 30) {                 // Instead of delay function (increase number = low rotation)
     tmr = millis();
-    val += dir;                               // Each step of servo
-    if (val >= 150 || val <= 0) dir = -dir;   // Servo rotation degree range
-    myServo.write(val);                        
+    i += dir;                                 // Each step of servo
+    if (i >= 170 || i <= 10) dir = -dir;      // Set servo rotation degree range
+    myServo.write(i);                        
     
     distance = calculateDistance();           // Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
     
-    Serial.print(val);                        // Sends the current degree into the Serial Port
+    Serial.print(i);                          // Sends the current degree into the Serial Port
     Serial.print(",");                        // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
     Serial.print(distance);                   // Sends the distance value into the Serial Port
-    Serial.println(".");                        // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+    Serial.print(".");                        // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
   }
 }
 
@@ -46,11 +46,11 @@ void loop() {
 // Function for calculating the distance measured by the Ultrasonic sensor
 int calculateDistance() { 
   
-  digitalWrite (trigPin, HIGH);           // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite (trigPin, HIGH);               // Sets the trigPin on HIGH state for 10 micro seconds
   delayMicroseconds(10);
   digitalWrite (trigPin, LOW);
   
-  duration = pulseIn(echoPin, HIGH);     // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);          // Reads the echoPin, returns the sound wave travel time in microseconds
   distance = duration / 58.2;
   return distance;
 }
