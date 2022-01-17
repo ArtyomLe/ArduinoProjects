c86fd1fe00c9d986a615bc64b7dbf3b8f5cb8e0c
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2022-01-17 09:29:45 +0200

    Update Radar.ino

--- Radar/Radar.ino
+++ Radar/Radar.ino
@@ -15,39 +15,39 @@ int distance;
 
 Servo myServo;                     // Creates a servo object for controlling the servo motor
 
+
 void setup() {
   pinMode(trigPin, OUTPUT);        // Sets the trigPin as an Output
   pinMode(echoPin, INPUT);         // Sets the echoPin as an Input
   Serial.begin(9600);
-  myServo.attach(12);              // Defines on which pin is the servo motor attached
+  myServo.attach(3);              // Defines on which pin is the servo motor attached
+  
+  uint32_t tmr;
+  int val = 15;
+  int dir = 1;
 }
+
+
 void loop() {
-  // rotates the servo motor from 15 to 165 degrees
-  for(int i=15; i<=165; i++){  
-  myServo.write(i);
-  delay(15);
-  distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
+  // асинхронный таймер на миллис
+  if (millis() - tmr >= 20) {
+    tmr = millis();
+    val += dir;                               // прибавляем скорость
+    if (val >= 165 || val <= 15) dir = -dir;  // разворачиваем
+    myservo.write(val)                        // применяем
+    
+    distance = calculateDistance();           // Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
   
-  Serial.print(i); // Sends the current degree into the Serial Port
-  Serial.print(","); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
-  Serial.print(distance); // Sends the distance value into the Serial Port
-  Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
-  }
-  
-  // Repeats the previous lines from 165 to 15 degrees
-  for(int i=165; i>15; i--){  
-  myServo.write(i);
-  delay(15);
-  distance = calculateDistance();
-  Serial.print(i);
-  Serial.print(",");
-  Serial.print(distance);
-  Serial.print(".");
+    Serial.print(val);                        // Sends the current degree into the Serial Port
+    Serial.print(",");                        // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+    Serial.print(distance);                   // Sends the distance value into the Serial Port
+    Serial.print(".");                        // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
   }
 }
 
+
 // Function for calculating the distance measured by the Ultrasonic sensor
-int calculateDistance(){ 
+int calculateDistance() { 
   
   digitalWrite(trigPin, LOW); 
   delayMicroseconds(2);
@@ -59,3 +59,31 @@ int calculateDistance(){
   distance= duration*0.034/2;
   return distance;
 }
+
+/*
+#include <Servo.h>
+Servo myservo;
+void setup() {
+  myservo.attach(3);
+}
+uint32_t tmr;    // переменная таймера
+int val = 15;    // переменная яркости
+int dir = 1;     // скорость и направление яркости
+void loop() {
+  // асинхронный таймер на миллис
+  if (millis() - tmr >= 20) {
+    tmr = millis();
+    val += dir;                               // прибавляем скорость
+    if (val >= 180 || val <= 15) dir = -dir;  // разворачиваем
+    myservo.write(val)                        // применяем
+    
+    distance = calculateDistance();           // Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
+  
+    Serial.print(val);                        // Sends the current degree into the Serial Port
+    Serial.print(",");                        // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+    Serial.print(distance);                   // Sends the distance value into the Serial Port
+    Serial.print(".");                        // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  }
+}
+*/
+
