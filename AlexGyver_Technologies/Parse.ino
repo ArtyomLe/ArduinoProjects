1e48d618977fd21b48a00291cfd2d1627a058eda
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2022-01-25 20:51:32 +0200

    Update Parse.ino

--- AlexGyver_Technologies/Parse.ino
+++ AlexGyver_Technologies/Parse.ino
@@ -12,4 +12,23 @@ void loop() {
     Serial.println(value);
   }
 }
-//================================================================================
+//======================================Управление 2 светодиодами=====================================
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(5);
+  pinMode(5, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 1) {     // Если получаем как минимум два значения в порт(a0-1023,b0-1023)
+    char key = Serial.read();       // Читаем и запоминаем символ
+    int value = Serial.parseInt();  // Парсим оставшиеся число
+    switch (key) {
+      case 'a': analogWrite(5, value);
+        break;
+      case 'b': analogWrite(10, value);
+        break;
+    }
+  }
+}
