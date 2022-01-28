b425490bae321951b185d3ff801df8f48ea5c26a
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2022-01-28 15:40:50 +0200

    Update Arduino_Sketch.ino

--- Processing/Meteo_Station/Arduino_Sketch.ino
+++ Processing/Meteo_Station/Arduino_Sketch.ino
@@ -1,1 +1,18 @@
-void() 
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()) {
+    char c = Serial.read();
+    switch(c) {
+      case 'n':
+      digitalWrite(13, HIGH);
+      break;
+      case 'f':
+      digitalWrite(13, LOW);
+      break;
+    }
+  }
+}
