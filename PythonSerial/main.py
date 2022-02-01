ecaf31ea1f7caafae4a84c7f856753a25e770767
ArtyomLe <60260330+ArtyomLe@users.noreply.github.com>
2022-02-01 17:19:36 +0200

    Update main.py

--- PythonSerial/main.py
+++ PythonSerial/main.py
@@ -14,3 +14,27 @@ portList  = []
 
 ui.show()
 app.exec()
+#======================================================================
+# Загрузка интерфеса из Qt Designer
+from PyQt5 import QtWidgets, uic
+# Связь с Ардуино по Serial ( с помощью библиотеки QSerialPort)
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5.QtCore import QIODevice
+
+app = QtWidgets.QApplication([])
+ui = uic.loadUi("Design.ui")
+ui.setWindowTitle("SerialGUI")
+
+serial = QSerialPort()
+serial.setBaudRate(115200)
+portList  = []
+
+ports = QSerialPortInfo().availablePorts()
+for port in ports:
+    portList.append(port.portName())
+
+ui.comL.addItems(portList)   # Выводим список портов в окне приложения
+
+ui.show()
+app.exec()
+#======================================================================
